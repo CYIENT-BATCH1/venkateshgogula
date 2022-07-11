8b2860f4cc74ea64b7229660d5a7c6b2072f5677
venkatesh995 <108973274+venkatesh995@users.noreply.github.com>
2022-07-11 16:45:52 +0530

    Create Day_8prob_5.c

---
+++ Day8/Day_8prob_5.c
@@ -0,0 +1,15 @@
+ Find out the value of T0PS2, T0PS1, T0PS0 bits in T0CON register if the value of the register is 0xF3
+
+#include <stdio.h>
+
+int main()
+{
+   int n=0xf3,x,i;
+   for(i=2;i>=0;i--){
+    x=(n>>i)&1;
+   
+   printf("%x",x);
+   }
+}
+   
+
